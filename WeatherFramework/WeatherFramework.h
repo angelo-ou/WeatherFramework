9597b9c15804d16019885feaa30d2efd0aa10f09
Angelo <aou@vmware.com>
2020-06-19 03:34:54 -0400

    Initial Commit

---
+++ WeatherFramework/WeatherFramework.h
@@ -0,0 +1,19 @@
+//
+//  WeatherFramework.h
+//  WeatherFramework
+//
+//  Created by Angelo Ou on 6/19/20.
+//  Copyright © 2020 Angelo Ou. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WeatherFramework.
+FOUNDATION_EXPORT double WeatherFrameworkVersionNumber;
+
+//! Project version string for WeatherFramework.
+FOUNDATION_EXPORT const unsigned char WeatherFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherFramework/PublicHeader.h>
+
+
